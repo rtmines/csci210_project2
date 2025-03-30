eb48a32aad6a845bb8e42d8e9fbf9dc96d4bfc2d
Ryan Tock <ryan_tock@mines.edu>
2025-03-30 16:04:30 -0600

    first submission

--- mkdirSplitpath.c
+++ mkdirSplitpath.c
@@ -3,27 +3,111 @@
 extern struct NODE* root;
 extern struct NODE* cwd;
 
+struct NODE* getChild(struct NODE* node, char* name) {
+    struct NODE* workingNode = node->childPtr;
+    if (workingNode == NULL) {
+        return NULL;
+    }
+
+    while (strcmp(name, workingNode->name) != 0) {
+        workingNode = workingNode->siblingPtr;
+        if (workingNode == NULL) {
+            return NULL;
+        }
+    }
+
+    return workingNode;
+}
+
 //make directory
 void mkdir(char pathName[]){
+    char path[strlen(pathName) + 1];
+    strcpy(path, pathName);
 
-    // TO BE IMPLEMENTED
-    //
-    // YOUR CODE TO REPLACE THE PRINTF FUNCTION BELOW
+    if (strlen(pathName) == 0) {
+        printf("MKDIR ERROR: no path provided\n");
+        return;
+    }
 
-    printf("TO BE IMPLEMENTED\n");
+    char baseName[strlen(pathName) + 1];
+    char dirName[strlen(pathName) + 1];
+
+    struct NODE* parentDir = splitPath(pathName, baseName, dirName);
+    if (parentDir == NULL) {
+        return;
+    }
+
+    if (getChild(parentDir, baseName) != NULL) {
+        printf("MKDIR ERROR: directory %s already exists\n", pathName);
+        return;
+    }
+
+    struct NODE* new = (struct NODE*)malloc(sizeof(struct NODE));
+
+    if (parentDir->childPtr == NULL) {
+        parentDir->childPtr = new;
+    } else {
+        struct NODE* workingNode = parentDir->childPtr;
+        while (workingNode->siblingPtr != NULL) {
+            workingNode = workingNode->siblingPtr;
+        }
+        workingNode->siblingPtr = new;
+    }
+
+    new->fileType = 'D';
+    new->parentPtr = parentDir;
+    strcpy(new->name, baseName);
+
+    printf("MKDIR SUCCESS: node %s successfully created\n", path);
 
     return;
 }
 
 //handles tokenizing and absolute/relative pathing options
 struct NODE* splitPath(char* pathName, char* baseName, char* dirName){
+    if (strcmp(pathName, "/") == 0) {
+        strcpy(dirName, pathName);
+        strcpy(pathName, "");
+        return NULL;
+    }
 
-    // TO BE IMPLEMENTED
-    // NOTE THAT WITHOUT COMPLETING THIS FUNCTION CORRECTLY
-    // rm, rmdir, ls, cd, touch COMMANDS WILL NOT EXECUTE CORRECTLY
-    // SEE THE PROVIDED SOLUTION EXECUTABLE TO SEE THEIR EXPECTED BEHAVIOR
+    struct NODE* node;
 
-    // YOUR CODE HERE
-    //
-    return NULL;
+    if (pathName[0] == '/') {
+        node = root;
+        strcpy(dirName, "/");
+        pathName++;
+    } else {
+        node = cwd;
+    }
+
+    const char delim[2] = "/";
+    char *token;
+    char *nextToken;
+
+    token = strtok(pathName, delim);
+    nextToken = strtok(NULL, delim);
+
+    
+    while (nextToken != NULL) {
+        strcat(dirName, token);
+        strcat(dirName, "/");
+
+        node = getChild(node, token);
+        if (node == NULL) {
+            printf("ERROR: directory %s does not exist\n", token);
+            return NULL;
+        }
+
+        token = nextToken;
+        nextToken = strtok(NULL, delim);
+    
+    }
+
+    strcpy(baseName, token);
+    if (strlen(dirName) > 0) {
+        dirName[strlen(dirName)-1] = '\0';
+    }
+    
+    return node;
 }
